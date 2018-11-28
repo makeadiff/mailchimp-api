930539a8382357f318d5b26b5c6d036acb976544
rhit99900 <rohitcse081@gmail.com>
2018-11-28 15:37:04 +0530

    CPP Update

--- get.php
+++ get.php
@@ -1,12 +1,11 @@
 <?php
 
-  if(isset($_GET['batch_id']))
+  // if(isset($_GET['batch_id']))
 
 
-  require('credentials.php');
-  $apiKey = $mailchimp_api_key;
+  include('credentials.php');
 
-  $dataCenter = substr($apiKey,strpos($apiKey,'-')+1);
+  $dataCenter = substr($mailchimp_api_key,strpos($mailchimp_api_key,'-')+1);
   $url = 'https://' . $dataCenter . '.api.mailchimp.com/3.0/batches?count=100';
 
   include('../curl.php');
@@ -14,14 +13,14 @@
   use \Curl\Curl;
 
   $ch = new Curl();
-  $ch->setOpt(CURLOPT_USERPWD, 'user:' . $apiKey);
+  $ch->setOpt(CURLOPT_USERPWD, 'user:' . $mailchimp_api_key);
   $ch->setOpt(CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
   $ch->get($url);
 
   if ($ch->error) {
       echo 'Error: ' . $ch->errorCode . ': ' . $ch->errorMessage . "\n".$url;
   } else {
-      var_dump($ch->response);
+      print json_encode($ch->response);
   }
 
 ?>
